a834c2cdf1c35a34721a278e96505a6f611655f8
AlameenAdesina <52167172+AlameenAdesina@users.noreply.github.com>
2025-07-03 19:24:56 -0400

    Update test_hmac.py

--- tests/test_hmac.py
+++ tests/test_hmac.py
@@ -6,3 +6,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from core import hmac_module  # Make sure core/ is a module (has __init__.py)
 
 # Add your test function below here and use "python tests/test_hmac.py" to run the test
+
+
+def test_hmac_valid():
+    key = b'supersecretkey1234567890123456'  # 32-byte key
+    message = b'This is a secret message'
+    tag = hmac_module.generate_hmac(key, message)
+    assert hmac_module.verify_hmac(key, message, tag)
+
+def test_hmac_invalid():
+    key = b'supersecretkey1234567890123456'
+    message = b'Valid message'
+    tampered = b'INVALID message'
+    tag = hmac_module.generate_hmac(key, message)
+    assert not hmac_module.verify_hmac(key, tampered, tag)
